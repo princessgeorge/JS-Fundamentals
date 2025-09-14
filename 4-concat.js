3408ff2eb5583aec8cef1f2224967dead5709365
AdaezeOkafor <princessgeorge093@gmail.com>
2025-09-14 09:43:27 -0700

    Fix concat.js to handle undefined properly

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
+
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-if (arg1 && arg2) {
-  console.log(`${arg1} is ${arg2}`);
-} else if (arg1) {
-  console.log(arg1);
-}
+console.log(arg1 + ' is ' + arg2);
